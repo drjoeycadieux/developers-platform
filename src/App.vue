d1b2570cbb12138955940840b8e7408b7e34641b
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-05-01 16:30:22 -0500

    Dev ship

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,22 @@
     <router-view />
 
     <FooterPage />
+    <hr />
+    <FooterMedia />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import FooterPage from "./components/FooterPage";
+import FooterMedia from "./components/FooterMedia";
 
 export default {
   name: "App",
   components: {
     Header,
     FooterPage,
+    FooterMedia,
   },
 };
 </script>
